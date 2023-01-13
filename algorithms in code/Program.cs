1895dac8eba9b1e0831b6bd6c14ef662f8ceebf8
metlebh <matlabyh@code.edu.az>
2023-01-13 09:26:17 +0400

    Duz olma umidi ile

---
+++ algorithms in code/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        private static int i;
+
+        static void Main(string[] args)
+        {
+
+
+            //int number = 76;
+            //int rem = 0;
+            //int sum = 0;
+            //while (number > 0)
+            //{
+            //    rem = number % 10;
+            //    sum = sum + rem;
+            //    number = number / 10;
+
+            //}
+            //if (sum % 2 == 0)
+            //{
+            //    Console.WriteLine("eded cutdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded tekdir");
+            //}
+
+
+            //int number = 100;
+            //int sum = 0;
+
+            //for (int i = 100; i > 0; i--)
+            //{
+            //    if (i % 13 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //        sum = sum + i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+
+
+
+            //int number = 987;
+            //int rem = 0;
+            //int min = number % 10;
+            //while (number > 0)
+            //{
+            //    rem = number % 10;
+            //    if (rem <= min)
+            //    {
+            //        min = rem;
+
+            //    }
+            //    number /= 10;
+            //}
+
+            //Console.WriteLine(min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
